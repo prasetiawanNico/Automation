5d5af7506d5667bcb89662bbc7ac0946b569de5b
nico_personal <prasetiawannico@gmail.com>
2023-04-02 15:31:09 +0700

    add mobile test case

--- Appium/src/test/java/apkDemo/MobileBrowserTest.java
+++ Appium/src/test/java/apkDemo/MobileBrowserTest.java
@@ -1,6 +1,7 @@
 package apkDemo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
@@ -9,9 +10,18 @@ public class MobileBrowserTest extends BrowserBaseTest{
 	@Test
 	public void browserTest() throws InterruptedException {
 		
-		driver.get("http://google.com");
-		driver.findElement(By.name("q")).sendKeys("Jambi");
-		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+//		driver.get("http://google.com");
+//		driver.findElement(By.name("q")).sendKeys("Jambi");
+//		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+//		Thread.sleep(9000);
+		
+		
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.xpath("//span[@class=\"navbar-toggler-icon\"]")).click();
+		
+		driver.findElement(By.cssSelector("a[routerlink*='products']")).click();
+		((JavascriptExecutor) driver).executeScript("window.scrollBy(0,1000)", ""); //scroll the page
+		driver.findElement(By.cssSelector("a[href*='products/3']")).click();
 		Thread.sleep(9000);
 		
 	}
