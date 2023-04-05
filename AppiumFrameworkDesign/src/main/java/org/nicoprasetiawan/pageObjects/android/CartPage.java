f9ceb9131a01ef00a4e7c7c7c39ef02eb76e7ee1
nico <prasetiawannico@gmail.com>
2023-04-05 11:19:00 +0700

    refactor testcase

--- AppiumFrameworkDesign/src/main/java/org/nicoprasetiawan/pageObjects/android/CartPage.java
+++ AppiumFrameworkDesign/src/main/java/org/nicoprasetiawan/pageObjects/android/CartPage.java
@@ -1,9 +1,14 @@
 package org.nicoprasetiawan.pageObjects.android;
 
+import java.nio.file.WatchEvent;
+import java.util.List;
+
 import org.nicoprasetiawan.utils.AndroidActions;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
 public class CartPage extends AndroidActions{
@@ -14,6 +19,58 @@ public class CartPage extends AndroidActions{
 		super(driver);
 		this.driver = driver;
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+	}
+	
+	
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/productPrice")
+	private List<WebElement> productList;
+	
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/totalAmountLbl")
+	private WebElement totalAmount;
+	
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/termsButton")
+	private WebElement terms;
+	
+	@AndroidFindBy(id = "android:id/button1")
+	private WebElement acceptButton;
+	
+	@AndroidFindBy(className = "android.widget.CheckBox")
+	private WebElement checkBox;
+	
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/btnProceed")
+	private WebElement proceed;
+	
+	
+	
+	public List<WebElement> getProductList() {
+		return productList;
+	}
+	
+	public double getProductSum() {
+		int productCount = productList.size();
+		double totalSum = 0;
+		
+		for (int i = 0; i < productCount; i++) {
+			String priceAmount = productList.get(i).getText();
+			Double price = getFormatedAmount(priceAmount, 1);;
+			totalSum = totalSum + price;
+		}
+		
+		return totalSum;
+	}
+	
+	public double getTotalAmountDisplayed() {
+		return getFormatedAmount(totalAmount.getText(), 1);
+	}
+	
+	public void acceptTermsConditions() {
+		longPress(terms);
+		acceptButton.click();
+	}
+	
+	public void submitOrder() {
+		checkBox.click();
+		proceed.click();
 	}
 
 }
--- AppiumFrameworkDesign/src/test/java/org/nicoprasetiawan/TestCase.java
+++ AppiumFrameworkDesign/src/test/java/org/nicoprasetiawan/TestCase.java
@@ -1,9 +1,19 @@
 package org.nicoprasetiawan;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.nicoprasetiawan.pageObjects.android.CartPage;
 import org.nicoprasetiawan.pageObjects.android.FormPage;
 import org.nicoprasetiawan.pageObjects.android.ProductCatalogue;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
 
 public class TestCase extends BaseTest {
 	
@@ -19,11 +29,20 @@ public class TestCase extends BaseTest {
 		productCatalogue.addItemToCartByIndex(0);
 		productCatalogue.addItemToCartByIndex(0);
 		CartPage cartPage = productCatalogue.goToCartPage();
-		
-		
+				
 		
 		Thread.sleep(3000);
-
+				
+		
+		double totalSum = cartPage.getProductSum();
+		double getTotalAmountDisplayed = cartPage.getTotalAmountDisplayed();
+		
+		Assert.assertEquals(totalSum, getTotalAmountDisplayed);
+		
+		cartPage.acceptTermsConditions();
+		cartPage.submitOrder();
+		
+		
 		
 	}
 }
